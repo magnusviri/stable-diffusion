ace39ccada5bae7b1478e8f3519d6580eb51ac2b
kejincai <woaicjn@gmail.com>
2022-12-17 08:37:28 +0900

    add:demo用脚本

---
+++ demo.py
@@ -0,0 +1,9 @@
+python scripts/preload_models.py
+python scripts/orig_scripts/txt2img.py
+python scripts/orig_scripts/txt2img.py \
+    - -prompt "a monkey riding a horse in outer space, like a knight" \
+    - -n_samples 1 - -n_iter 1 - -plms
+
+python scripts/orig_scripts/txt2img.py \
+    - -prompt "the king of monkey is fishing in the office of shanghai" \
+    - -n_samples 1 - -n_iter 1 - -plms
